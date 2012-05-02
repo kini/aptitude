de9dc2f4c2a877a30a1cac2836c44066b4ba0235
Daniel Hartwig <mandyke@gmail.com>
2012-05-02 14:07:27 +0800

    Fix check_boost.sh for out-of-tree builds

--- check_boost.sh
+++ check_boost.sh
@@ -18,20 +18,28 @@ BOOST_PATTERN='boost/[a-zA-Z0-9_./-]*\.hpp'
 # Exclude headers that are only used, e.g., to write configure tests.
 BOOST_CONFIGURE_PATTERN="$BOOST_PATTERN"' dnl$'
 
+SRCDIR=${srcdir:-.}
+
+if [ ! -f $SRCDIR/configure.ac ]
+then
+  echo "Could not locate configure.ac."
+  exit 2
+fi
+
 # Check that the source code and the configure check look for the same
 # Boost headers.
-SRC_OCCURRENCES=$((find src \( -name \*.cc -or -name \*.h \) -print0; find tests \( -name \*.cc -or -name \*.h \) -print0) \
+SRC_OCCURRENCES=$((find $SRCDIR/src \( -name \*.cc -or -name \*.h \) -print0; find $SRCDIR/tests \( -name \*.cc -or -name \*.h \) -print0) \
                   | xargs -0 grep -h --only-matching "$BOOST_PATTERN" | sort -u)
 
-if ! grep -h --only-matching "$BOOST_CONFIGURE_PATTERN" configure.ac | sed 's/ dnl$//' | sort -c
+if ! grep -h --only-matching "$BOOST_CONFIGURE_PATTERN" $SRCDIR/configure.ac | sed 's/ dnl$//' | sort -c
 then
   echo "The list of Boost headers in configure.ac is not sorted."
   RESULT=2
 fi
 
-CONFIGURE_OCCURRENCES=$(grep -h --only-matching "$BOOST_CONFIGURE_PATTERN" configure.ac | sed 's/ dnl$//' | sort -u)
+CONFIGURE_OCCURRENCES=$(grep -h --only-matching "$BOOST_CONFIGURE_PATTERN" $SRCDIR/configure.ac | sed 's/ dnl$//' | sort -u)
 
-if ! cmp <(echo "$SRC_OCCURRENCES") <(echo "$CONFIGURE_OCCURRENCES")
+if ! cmp <(echo "$SRC_OCCURRENCES") <(echo "$CONFIGURE_OCCURRENCES") >/dev/null
 then
     ONLY_SRC=$(comm -2 -3 <(echo "$SRC_OCCURRENCES") <(echo "$CONFIGURE_OCCURRENCES"))
     ONLY_CONFIG=$(comm -1 -3 <(echo "$SRC_OCCURRENCES") <(echo "$CONFIGURE_OCCURRENCES"))
