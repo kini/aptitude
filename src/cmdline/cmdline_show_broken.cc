2b3d25695f6645bee4f0e8bf879c621ce97f4df3
Manuel A. Fernandez Montecelo <manuel.montezelo@gmail.com>
2015-10-06 17:23:50 +0100

    Minor improvements to documentation, const safety and style/readability

--- src/cmdline/cmdline_show_broken.cc
+++ src/cmdline/cmdline_show_broken.cc
@@ -1,86 +1,84 @@
 // cmdline_show_broken.cc
 //
 //   Copyright 2004 Daniel Burrows
+//   Copyright 2015 Manuel A. Fernandez Montecelo
 
 #include "cmdline_show_broken.h"
 
 #include "cmdline_common.h"
+#include "aptitude.h"
+#include "generic/apt/apt.h"
 
-#include <aptitude.h>
-
-#include <generic/apt/apt.h>
-
-#include <apt-pkg/pkgcache.h>
-
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
 
 using namespace std;
 
-void show_broken_deps(pkgCache::PkgIterator pkg)
+void show_broken_deps(const pkgCache::PkgIterator& pkg)
 {
-  const unsigned int indent=pkg.FullName(true).size() + 3;
-  bool is_first_dep=true;
-  pkgCache::VerIterator ver=(*apt_cache_file)[pkg].InstVerIter(*apt_cache_file);
+  string pkg_name_segment = string(" ") + pkg.FullName(true) + " :";
+  const unsigned int indent = pkg_name_segment.length();
+  cout << pkg_name_segment;
 
-  cout << " " << pkg.FullName(true) << " :";
-
-  if(ver.end() == true)
+  pkgCache::VerIterator ver = (*apt_cache_file)[pkg].InstVerIter(*apt_cache_file);
+  if (ver.end() == true)
     {
       cout << endl;
       return;
     }
 
-  for(pkgCache::DepIterator dep=ver.DependsList(); !dep.end(); ++dep)
-    {
-      pkgCache::DepIterator first=dep, prev=dep;
+  bool is_first_dep = true;
 
-      while(dep->CompareOp & pkgCache::Dep::Or)
+  for (pkgCache::DepIterator dep = ver.DependsList(); !dep.end(); ++dep)
+    {
+      pkgCache::DepIterator first = dep, prev = dep;
+
+      while (dep->CompareOp & pkgCache::Dep::Or)
 	++dep;
 
       // Yep, it's broken.
-      if(dep.IsCritical() &&
+      if (dep.IsCritical() &&
 	 !((*apt_cache_file)[dep]&pkgDepCache::DepGInstall))
 	{		  
-	  bool is_first_of_or=true;
+	  bool is_first_of_or = true;
 	  // Iterate over the OR group, print out the information.
 
 	  do
 	    {
-	      if(!is_first_dep)
-		for(unsigned int i=0; i<indent; ++i)
+	      if (!is_first_dep)
+		for (unsigned int i = 0; i < indent; ++i)
 		  printf(" ");
 
-	      is_first_dep=false;
+	      is_first_dep = false;
 
-	      if(!is_first_of_or)
-		for(unsigned int i=0; i<strlen(dep.DepType())+3; ++i)
+	      if (!is_first_of_or)
+		for (unsigned int i = 0; i < (strlen(dep.DepType()) + 3); ++i)
 		  printf(" ");
 	      else
 		printf(" %s: ", first.DepType());
 
-	      is_first_of_or=false;
+	      is_first_of_or = false;
 
               cout << first.TargetPkg().FullName(true);
 
-	      if(first.TargetVer())
+	      if (first.TargetVer())
 		printf(" (%s %s)", first.CompType(), first.TargetVer());
 
 	      // FIXME: handle virtual packages sanely.
-	      pkgCache::PkgIterator target=first.TargetPkg();
+	      pkgCache::PkgIterator target = first.TargetPkg();
 	      // Don't skip real packages which are provided.
-	      if(!target.VersionList().end())
+	      if (!target.VersionList().end())
 		{
 		  printf(" ");
 
-		  pkgCache::VerIterator ver=(*apt_cache_file)[target].InstVerIter(*apt_cache_file);
+		  pkgCache::VerIterator ver = (*apt_cache_file)[target].InstVerIter(*apt_cache_file);
 
-		  if(!ver.end()) // ok, it's installable.
+		  if (!ver.end()) // ok, it's installable.
 		    {
-		      if((*apt_cache_file)[target].Install())
+		      if ((*apt_cache_file)[target].Install())
 			printf(_("but %s is to be installed."),
 			       ver.VerStr());
-		      else if((*apt_cache_file)[target].Upgradable())
+		      else if ((*apt_cache_file)[target].Upgradable())
 			printf(_("but %s is installed and it is kept back."),
 			       target.CurrentVer().VerStr());
 		      else
@@ -90,24 +88,26 @@ void show_broken_deps(pkgCache::PkgIterator pkg)
 		  else
 		    {
 		      pkgCache::VerIterator cand = (*apt_cache_file)[target].CandidateVerIter(*apt_cache_file);
-		      if(cand.end())
+		      if (cand.end())
 			printf(_("but it is not installable."));
 		      else
 			printf(_("but it is not going to be installed."));
 		    }
 		}
 	      else
-		// FIXME: do something sensible here!
-		printf(_(" which is a virtual package."));
+		{
+		  // FIXME: do something sensible here!
+		  printf(_(" which is a virtual package."));
+		}
 
-	      if(first!=dep)
+	      if (first != dep)
 		printf(_(" or"));
 
 	      printf("\n");
 
-	      prev=first;
+	      prev = first;
 	      ++first;
-	    } while(prev!=dep);
+	    } while (prev != dep);
 	}
     }
 }
@@ -115,18 +115,17 @@ void show_broken_deps(pkgCache::PkgIterator pkg)
 bool show_broken()
 {
   pkgvector broken;
-  for(pkgCache::PkgIterator i=(*apt_cache_file)->PkgBegin();
-      !i.end(); ++i)
+  for (pkgCache::PkgIterator i = (*apt_cache_file)->PkgBegin(); !i.end(); ++i)
     {
-      if((*apt_cache_file)[i].InstBroken())
+      if ((*apt_cache_file)[i].InstBroken())
 	broken.push_back(i);
     }
 
-  if(!broken.empty())
+  if (!broken.empty())
     {
       printf(_("The following packages have unmet dependencies:\n"));
 
-      for(pkgvector::iterator pkg=broken.begin(); pkg!=broken.end(); ++pkg)
+      for (pkgvector::const_iterator pkg = broken.begin(); pkg != broken.end(); ++pkg)
 	show_broken_deps(*pkg);
       return false;
     }
--- src/cmdline/cmdline_show_broken.h
+++ src/cmdline/cmdline_show_broken.h
@@ -1,24 +1,30 @@
 // cmdline_show_broken.h                  -*-c++-*-
 //
 //   Copyright 2004 Daniel Burrows
+//   Copyright 2015 Manuel A. Fernandez Montecelo
 
 #ifndef CMDLINE_SHOW_BROKEN_H
 #define CMDLINE_SHOW_BROKEN_H
 
 #include <apt-pkg/pkgcache.h>
 
+
 /** \file cmdline_show_broken.h
  */
 
-/** Shows a list of all broken packages together with their
- *  dependencies.  Similar to and based on the equivalent routine in
- *  apt-get.
+/** Shows a list of all broken packages together with their dependencies.
+ *  Similar to and based on the equivalent routine in apt-get.
  *
- *  Returns \b false if some packages are broken.
+ *  @return \b false if some packages are broken.
  */
 bool show_broken();
 
-/** Shows broken dependencies for a single package */
-void show_broken_deps(pkgCache::PkgIterator pkg);
+
+/** Shows broken dependencies for a single package
+ *
+ * @pkg The package
+ */
+void show_broken_deps(const pkgCache::PkgIterator& pkg);
+
 
 #endif // CMDLINE_SHOW_BROKEN
