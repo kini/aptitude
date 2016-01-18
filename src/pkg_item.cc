e7f64254302bae7a119442a0a2b55ff3ace427ae
Manuel A. Fernandez Montecelo <manuel.montezelo@gmail.com>
2016-01-18 20:08:17 +0000

    [curses] "Install" on virtual packages now installs single candidate, or [error] (Closes: #174820)
    [curses] "Install" on virtual packages now installs single candidate, or
    emits an error if there are no candidates or more than one (Closes: #174820)

--- src/pkg_item.cc
+++ src/pkg_item.cc
@@ -24,6 +24,7 @@
 #include <cwidget/config/keybindings.h>
 #include <cwidget/dialogs.h>
 #include <cwidget/fragment.h>
+#include <cwidget/generic/util/ssprintf.h>
 #include <cwidget/generic/util/transcode.h>
 #include <cwidget/toplevel.h>
 
@@ -123,7 +124,39 @@ void pkg_item::do_highlighted_changed(bool highlighted)
 
 void pkg_item::do_select(undo_group *undo)
 {
-	(*apt_cache_file)->mark_install(package, aptcfg->FindB(PACKAGE "::Auto-Install", true), false, undo);
+  pkgCache::PkgIterator& package_to_install = package;
+
+  if (is_virtual(package))
+    {
+      std::string pkgtext;
+      std::vector<std::string> pkgnames;
+      for (pkgCache::PrvIterator prv = package.ProvidesList(); !prv.end(); ++prv)
+	{
+	  string name = prv.OwnerPkg().FullName(true);
+	  pkgnames.push_back(name);
+
+	  pkgtext += cwidget::util::ssprintf("* %s\n", name.c_str());
+	}
+
+      // messages copied from from cmdline_action.cc
+      if (pkgnames.empty())
+	{
+	  _error->Error(_("\"%s\" exists in the package database, but it is not a real package and no package provides it\n"),
+			package.FullName(true).c_str());
+	}
+      else if (pkgnames.size() == 1)
+	{
+	  // only one, install this one
+	  package_to_install = package.ProvidesList().OwnerPkg();
+	}
+      else
+	{
+	  _error->Error(_("\"%s\" is a virtual package provided by:\n\n%s\nYou must choose one to install.\n"),
+			package.FullName(true).c_str(), pkgtext.c_str());
+	}
+    }
+
+  (*apt_cache_file)->mark_install(package_to_install, aptcfg->FindB(PACKAGE "::Auto-Install", true), false, undo);
 }
 
 void pkg_item::select(undo_group *undo)
