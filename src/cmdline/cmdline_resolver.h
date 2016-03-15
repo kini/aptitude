95aafa21a716a95848a0586c2e9dc16b18d7ce59
Manuel A. Fernandez Montecelo <manuel.montezelo@gmail.com>
2016-03-15 01:19:50 +0000

    Update copyright years

--- src/cmdline/cmdline_resolver.h
+++ src/cmdline/cmdline_resolver.h
@@ -1,6 +1,7 @@
 // cmdline_resolver.h                              -*-c++-*-
 //
 //   Copyright (C) 2005, 2007-2008, 2010 Daniel Burrows
+//   Copyright (C) 2014-2016 Manuel A. Fernandez Montecelo
 //
 //   This program is free software; you can redistribute it and/or
 //   modify it under the terms of the GNU General Public License as
