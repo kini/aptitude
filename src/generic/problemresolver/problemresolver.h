e5f87181c07b9123258150e35200273986c77860
Manuel A. Fernandez Montecelo <manuel.montezelo@gmail.com>
2016-04-20 00:30:02 +0100

    Add copyright information

--- src/generic/problemresolver/problemresolver.h
+++ src/generic/problemresolver/problemresolver.h
@@ -1,6 +1,7 @@
 // problemresolver.h                  -*-c++-*-
 //
 //   Copyright (C) 2005, 2007-2010 Daniel Burrows
+//   Copyright (C) 2015 Manuel A. Fernandez Montecelo
 //
 //   This program is free software; you can redistribute it and/or
 //   modify it under the terms of the GNU General Public License as
